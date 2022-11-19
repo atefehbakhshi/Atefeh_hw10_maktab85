8b8a5707ea7601b09646de5847853363a8f04619
Atefeh Bakhshi <at.bakhshi1373@gmail.com>
2022-11-19 17:58:12 +0330

    feat: add closing and opening menu

---
+++ Q7/script.js
@@ -0,0 +1,4 @@
+'use strict'
+let buttonMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu');
+buttonMenu.addEventListener('click', () => menu.classList.toggle('hidden'))
